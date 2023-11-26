6c76adcbb7ae04f9b5feadb09062819213374c10
wanwensheng1 <13998094492@163.com>
2023-11-26 15:30:48 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author: wanws
+ * @date: 2023/11/26 15:27
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
